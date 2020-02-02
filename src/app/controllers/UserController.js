0075bc49a32ca47c65d9a2d1ec749214b07cc8fa
Guilherme Bublitz <bublitz.stein@gmail.com>
2020-02-01 23:11:18 -0300

    Adds update user controller

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -14,8 +14,24 @@ class UserController {
   }
 
   async update(req, res) {
-    console.log(req.userId);
-    return res.json({ message: 'ola enfermeira' });
+    const { email, oldPassword } = req.body;
+    const user = await User.findByPk(req.userId);
+
+    if (email && email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name } = await user.update(req.body);
+
+    return res.json({ id, name, email });
   }
 }
 
