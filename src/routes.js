6fb4b977627e857a7a9f01688a931877c65b1706
Guilherme Bublitz <bublitz.stein@gmail.com>
2020-02-01 22:13:36 -0300

    Adds db connection to model and routes

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/users', (req, res) => {
-  res.json({ message: "ola enfermeira"});
+routes.get('/users', async (req, res) => {
+  const user = await User.create({
+    name: 'Guibas',
+    email: 'b@b.com',
+    password: '123456',
+  });
+  res.json(user);
 });
 
 export default routes;
