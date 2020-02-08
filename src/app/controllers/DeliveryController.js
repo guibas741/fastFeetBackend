e7506236a5f810f078029b8e1e1acb0639910d1e
Guilherme Bublitz <bublitz.stein@gmail.com>
2020-02-08 08:42:01 -0300

    Creates update delivery route

--- src/app/controllers/DeliveryController.js
+++ src/app/controllers/DeliveryController.js
@@ -31,20 +31,13 @@ class DeliveryController {
       recipient_id: Yup.number().required(),
       deliveryman_id: Yup.number().required(),
       signature_id: Yup.number(),
-      start_date: Yup.date().required(),
     });
 
     if (!(await schema.isValid(req.body))) {
       return res.status(400).json({ error: 'Validation fails' });
     }
 
-    const {
-      product,
-      recipient_id,
-      deliveryman_id,
-      signature_id,
-      start_date,
-    } = req.body;
+    const { product, recipient_id, deliveryman_id, signature_id } = req.body;
 
     const existsRecipient = await Recipient.findByPk(recipient_id);
 
@@ -63,10 +56,63 @@ class DeliveryController {
       recipient_id,
       deliveryman_id,
       signature_id,
-      start_date,
     });
 
     return res.json(delivery);
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      product: Yup.string(),
+      recipient_id: Yup.number(),
+      deliveryman_id: Yup.number(),
+      signature_id: Yup.number(),
+      start_date: Yup.date(),
+      end_date: Yup.date(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { id } = req.params;
+    const delivery = await Delivery.findByPk(id);
+
+    if (!delivery) {
+      return res.status(400).json({ error: 'Delivery does not exists' });
+    }
+
+    const {
+      product,
+      recipient_id,
+      deliveryman_id,
+      signature_id,
+      start_date,
+      end_date,
+    } = req.body;
+
+    const existsRecipient = await Recipient.findByPk(recipient_id);
+
+    if (!existsRecipient) {
+      return res.status(401).json({ error: 'Recipient does not exists' });
+    }
+
+    const existsDeliveryman = await Deliveryman.findByPk(deliveryman_id);
+
+    if (!existsDeliveryman) {
+      return res.status(401).json({ error: 'Deliveryman does not exists' });
+    }
+
+    const deliveryUpdated = await delivery.update({
+      product,
+      recipient_id,
+      deliveryman_id,
+      signature_id,
+      start_date,
+      end_date,
+    });
+
+    return res.json(deliveryUpdated);
   }
 }
 
